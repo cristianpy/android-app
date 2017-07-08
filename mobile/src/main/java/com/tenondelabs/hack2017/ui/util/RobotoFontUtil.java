a6c0f86b9131056ab94303c8a8f2a649378fdd16
Rodrigo Garcete <oneosmar89@gmail.com>
2017-07-08 15:57:07 -0400

    Add class util for Roboto font

---
+++ mobile/src/main/java/com/tenondelabs/hack2017/ui/util/RobotoFontUtil.java
@@ -0,0 +1,34 @@
+package com.tenondelabs.hack2017.ui.util;
+
+import android.content.Context;
+import android.graphics.Typeface;
+
+/**
+ * @author Rodrigo Garcete
+ * @version 1.0
+ * Clase Util para Roboto Font
+ * Copyright 2015 Wigo Creative. All rights reserved
+ */
+public class RobotoFontUtil {
+
+    private static Typeface typefaceRR;
+    private static Typeface typefaceRL;
+
+    public static Typeface getFontRobotoRegular(Context context){
+        if (typefaceRR == null){
+            typefaceRR = Typeface.createFromAsset(context.getAssets(),
+                    "font/Roboto-Regular.ttf");
+        }
+
+        return typefaceRR;
+    }
+
+    public static Typeface getFontRobotoLigth(Context context){
+        if (typefaceRL == null){
+            typefaceRL = Typeface.createFromAsset(context.getAssets(),
+                    "font/Roboto-Light.ttf");
+        }
+
+        return typefaceRL;
+    }
+}
