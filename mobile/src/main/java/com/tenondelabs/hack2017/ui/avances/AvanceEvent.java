4a399157d129d903963899c06c88188e29d241b5
Rodrigo Garcete <oneosmar89@gmail.com>
2017-07-08 23:08:53 -0400

    Add fragments and presenters

---
+++ mobile/src/main/java/com/tenondelabs/hack2017/ui/avances/AvanceEvent.java
@@ -0,0 +1,35 @@
+package com.tenondelabs.hack2017.ui.avances;
+
+
+import com.tenondelabs.hack2017.data.model.Gobernacion;
+
+import java.util.List;
+
+/**
+ * @author Rodrigo Garcete
+ * @version 1.0
+ * Class target for event
+ * Copyright 2016 Akibusca. All rights reserved
+ */
+public class AvanceEvent {
+
+    private String error;
+    private List<Gobernacion> gobernaciones;
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public List<Gobernacion> getGobernaciones() {
+        return gobernaciones;
+    }
+
+    public void setGobernaciones(List<Gobernacion> gobernaciones) {
+        this.gobernaciones = gobernaciones;
+    }
+
+}
